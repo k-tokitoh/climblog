4817e2247b8e5faf109ef82231af9a3321c4dee5
k-tokitoh <tsuzuki670@gmail.com>
2018-11-10 12:14:09 +0000

    写真がない場合は課題及びログが作成されないよう修正した

--- app/controllers/problems_controller.rb
+++ app/controllers/problems_controller.rb
@@ -20,16 +20,53 @@ class ProblemsController < ApplicationController
     @problem = Problem.new(problem_params)
     @log = Log.new(log_params)
     @spot = @problem.spot
-    if log_params[:photos].present? && @problem.save
-      @log = Log.new(log_params)
-      @log.problem_id = @problem.id
-      if @log.save
-        redirect_to spot_path(@problem.spot)
-        return
+    @log.problem_id = Problem.first.id
+    
+    # logをsaveできなかった場合、登録画面に戻る
+    if !@log.save
+      render :new
+    # logをsaveできた場合を以下で考える
+    else
+      # logが写真を持っていなかった場合、logを取り消して登録画面に戻る
+      if !@log.photos.first
+        @log.destroy
+        flash.now[:danger] = '新規課題の登録には写真の添付が必要です。'
+        render :new
+      # logが写真を持っていた場合を以下で考える
+      else
+        # problemをsaveできなかった場合、logを取り消して登録画面に戻る
+        if !@problem.save
+          @log.destroy
+          render :new
+        # problemをsaveできた場合、その課題の詳細画面に遷移する
+        else
+          Log.find(@log.id).update(problem_id: @problem.id)
+          flash[:success] = '課題とログを登録しました。'
+          redirect_to problem_path(@problem)
+        end
       end
-    end
-    flash.now[:danger] = '新規課題の登録には写真の添付が必要です。'
-    render :new
+    end  
+    #   if @log.photos
+    #     if @problem.save
+    #       redirect_to spot_path(@problem.spot)
+    #       return
+    #     end
+    #   else
+    #     @log.destroy
+    #     @log = Log.new(log_params)
+    #   end
+    # end
+    # flash.now[:danger] = '新規課題の登録には写真の添付が必要です。'
+    # render :new
+    # if log_params[:photos].present? && @problem.save
+    #   @log = Log.new(log_params)
+    #   @log.problem_id = @problem.id
+    #   if @log.save
+    #     redirect_to spot_path(@problem.spot)
+    #     return
+    #   end
+    # end
+    
   end
 
   def destroy
